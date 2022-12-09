0d960927942e746f54d8527300b3ba66b489769d
jelfes <janelfes@gmail.com>
2022-12-09 19:21:23 +0100

    update save_replies function to get more replies and save data

--- Twitter/save_replies.py
+++ Twitter/save_replies.py
@@ -1,12 +1,36 @@
 import helpers
+import time
 import pandas as pd
 
 hp = helpers.helpers()
 
-p = {"max_results": 100}
-id = "1598431890960162847"
+max_tweets = {"max_results": 20}
+max_replies = {"max_results": 100}
 
-replies = hp.get_replies(conversation_id=id, params=p)
+# get user id
+usrid = hp.get_user_id("GiorgiaMeloni")
 
-df = pd.DataFrame(replies["data"])
-df.to_csv(f"replies_{id}.csv")
+# get latest tweets
+tweets = hp.get_user_tweets(usrid, params=max_tweets)
+
+
+for i, tweet in enumerate(tweets["data"]):
+    id = tweet["id"]
+    tweet_df = pd.DataFrame(tweet)
+
+    # get replies to the tweets
+    replies = hp.get_replies(conversation_id=id, params=max_replies)
+    replies = replies.get("data")
+
+    if replies is not None:
+        replies_df = pd.DataFrame(replies)
+        tweet_df.to_csv(f"data/tweet_{id}.csv")
+        replies_df.to_csv(f"data/replies_{id}.csv")
+
+        print(f'Saved \t\t{i+1}/{len(tweets["data"])} Tweets.')
+    else:
+        print(f'Skipped \t{i+1}/{len(tweets["data"])} Tweets (not enough replies).')
+    # time.sleep(5)
+
+print("\n ----------------------------------------- \n")
+print("Done!")
